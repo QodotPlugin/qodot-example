404e79e44d6fbe8d357452a029493c03b4248dd2
Josh Palmer <jpalmerwatkins@gmail.com>
2020-02-19 22:00:17 +0000

    Update example content

--- scripts/OrbitArm.gd
+++ scripts/OrbitArm.gd
@@ -1,4 +1,9 @@
+class_name OrbitArm
 extends Spatial
+
+export(bool) var translate := true
+export(bool) var rotate := true
+export(bool) var zoom := true
 
 var yaw = 0.0
 var pitch = 0.0
@@ -14,14 +19,17 @@ func _process(delta):
 
 func _input(event) -> void:
 	if event is InputEventMouseButton:
+		if not zoom: return
 		if event.button_index == BUTTON_WHEEL_UP:
 			$Camera.translation.z -= 2.0
 		elif event.button_index == BUTTON_WHEEL_DOWN:
 			$Camera.translation.z += 2.0
 	elif event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(BUTTON_MASK_LEFT):
+			if not translate: return
 			translation += $Camera.global_transform.basis.x * event.relative.x * -0.05
 			translation += $Camera.global_transform.basis.y * event.relative.y * 0.05
 		elif Input.is_mouse_button_pressed(BUTTON_MASK_RIGHT):
+			if not rotate: return
 			yaw += event.relative.x * -0.002
 			pitch += event.relative.y * -0.002
