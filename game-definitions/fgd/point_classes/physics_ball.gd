e5c0b71d5746b5a39599203c71fa8c738315dd5e
Josh Palmer <jpalmerwatkins@gmail.com>
2020-03-17 00:59:43 +0000

    Add velocity property to physics ball

--- game-definitions/fgd/point_classes/physics_ball.gd
+++ game-definitions/fgd/point_classes/physics_ball.gd
@@ -1,6 +1,17 @@
 extends RigidBody
+tool
 
-export(Dictionary) var properties
+export(Dictionary) var properties setget set_properties
+
+func set_properties(new_properties : Dictionary) -> void:
+	if(properties != new_properties):
+		properties = new_properties
+		update_properties()
+
+func update_properties():
+	print(properties)
+	if 'velocity' in properties:
+		linear_velocity = properties['velocity']
 
 func use():
 	bounce()
